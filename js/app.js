a88e8348969c8f11381d92a64257db57f6bf3f8b
Steven Starwalt <steven.starwalt@yahoo.com>
2018-01-29 14:34:53 -0800

    created the edge cell array

--- js/app.js
+++ js/app.js
@@ -1,36 +1,23 @@
 'use strict';
 
 var game = document.getElementById('gameTable');
-// var number = document.getElementById('numberTable');
-var gameSize = 5;
+
+var gameSize = 3;
+
 var tableTotal = 0;
-// var row = ['zero', 'one', 'two', 'three', 'four', 'five', 'six'];
-// var cell = ['0', '1', '2', '3', '4', '5', '6'];
+
 var gameNumbers = [];
+
 var cellNumber = 0;
+
+var topCells = [];
+var rightCells = [];
+var bottomCells = [];
+var leftCells = [];
 
 function randomNumber() {
   return Math.floor(Math.random() * Math.floor(3) + 1);
 }
-
-// function makeNumberTable(){
-//   for (var i = 1; i < gameSize + 1; i++) {
-//     var trEl = document.createElement('tr');
-//     for(var j = 1; j < gameSize + 1; j++){
-//       var tdEl = document.createElement('td');
-//       tdEl.setAttribute('id', row[i] + ' ' + cell[j]);
-//       var cellNumber = randomNumber();
-//       tableTotal = tableTotal + cellNumber;
-//       tdEl.textContent = cellNumber;
-//       trEl.appendChild(tdEl);
-//     }
-//     number.appendChild(trEl);
-//   }
-//   console.log(tableTotal);
-//   if(tableTotal > (gameSize * gameSize * 2)){
-//     location.reload();
-//   }
-// }
 
 function makeGameTable(){
   for (var i = 1; i < gameSize + 1; i++) {
@@ -55,5 +42,29 @@ function makeGameTable(){
   }
 }
 
+function edgeCells() {
+  var edge = 0;
+  for(var i = 1; i < gameSize + 1; i++){
+    edge++;
+    topCells.push(edge);
+  }
+  rightCells.push(edge);
+  for(var j = 1; j < gameSize; j++){
+    edge = edge + gameSize;
+    rightCells.push(edge);    
+  }
+  edge = 1;
+  leftCells.push(edge);
+  for(var k = 1; k < gameSize; k++){
+    edge = edge + gameSize;
+    leftCells.push(edge);  
+  }
+  bottomCells.push(edge);
+  for(var l = 1; l < gameSize; l++){
+    edge++;
+    bottomCells.push(edge);
+  }
+}
+
 makeGameTable();
-// makeNumberTable();
+edgeCells();
