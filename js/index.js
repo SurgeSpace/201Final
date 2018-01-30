ac62f93293d3187622d9791a8e3cf2153ca19eba
Josiah Green <JosiahGreen90@gmail.com>
2018-01-29 17:08:41 -0800

    fixed local storage bug on refresh

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
-document.getElementById('button').style.display = 'none';
+// document.getElementById('button').style.display = 'none';
 
 // on form submit, store data
 function store(){
-  var inputFirstName = document.getElementById('firstName');
-  var inputLastName = document.getElementById('lastName');
-  localStorage.setItem('firstName', inputFirstName.value);
-  localStorage.setItem('lastName', inputLastName.value);
+  if (!localStorage.firstName) {
+    var inputFirstName = document.getElementById('firstName');
+    var inputLastName = document.getElementById('lastName');
+    localStorage.setItem('firstName', inputFirstName.value);
+    localStorage.setItem('lastName', inputLastName.value);
+  }
 }
 
 function hideEl() {
@@ -15,15 +17,10 @@ function hideEl() {
   document.getElementById('fText').style.display = 'none';
 }
 
-function showButton() {
-  document.getElementById('button').style.display = 'inline';
-}
-
 function pageLoad() {
   if (localStorage.firstName) {
     document.getElementById('welcome').innerHTML = 'Welcome ' + localStorage.firstName + ' ' + localStorage.lastName;
     hideEl();
-    showButton();
   }
 }
 
