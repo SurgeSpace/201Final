a72d2e7bd0322f3b2a3b93b23da328c7b8813b8a
Steven Starwalt <steven.starwalt@yahoo.com>
2018-01-29 18:01:22 -0800

    treaked with the game board

--- js/app.js
+++ js/app.js
@@ -2,27 +2,30 @@
 
 var game = document.getElementById('gameTable');
 
-var gameSize = 3;
+var gameSize = 5;
 
 var tableTotal = 0;
 
 var gameNumbers = [];
 
-var cellNumber = 0;
+var cellNumber = -1;
 
 var topCells = [];
 var rightCells = [];
 var bottomCells = [];
 var leftCells = [];
 
+var rowLength = parseInt(gameSize);
+var clickCell = 0;
+
 function randomNumber() {
   return Math.floor(Math.random() * Math.floor(3) + 1);
 }
 
 function makeGameTable(){
-  for (var i = 1; i < gameSize + 1; i++) {
+  for (var i = 0; i < gameSize; i++) {
     var trEl = document.createElement('tr');
-    for(var j = 1; j < gameSize + 1; j++){
+    for(var j = 0; j < gameSize; j++){
       cellNumber++;
       var tdEl = document.createElement('td');
       var button = document.createElement('button');
@@ -44,7 +47,8 @@ function makeGameTable(){
 
 function edgeCells() {
   var edge = 0;
-  for(var i = 1; i < gameSize + 1; i++){
+  topCells.push(edge);
+  for(var i = 0; i < gameSize - 1; i++){
     edge++;
     topCells.push(edge);
   }
@@ -53,7 +57,7 @@ function edgeCells() {
     edge = edge + gameSize;
     rightCells.push(edge);    
   }
-  edge = 1;
+  edge = 0;
   leftCells.push(edge);
   for(var k = 1; k < gameSize; k++){
     edge = edge + gameSize;
@@ -64,7 +68,81 @@ function edgeCells() {
     edge++;
     bottomCells.push(edge);
   }
+  topCells.pop();
+  topCells.shift();
+  rightCells.pop();
+  rightCells.shift();
+  leftCells.pop();
+  leftCells.shift();
+  bottomCells.pop();
+  bottomCells.shift();
 }
+
+function updateNumbers(event){
+  clickCell = parseInt(event.target.id);
+  gameNumbers[clickCell] = gameNumbers[clickCell] + 1;
+  clearAndCheck();
+  console.log(gameNumbers);
+}
+
+function clearAndCheck(){
+  for(var i in gameNumbers){
+    if(gameNumbers[i] > 3){
+      event.target.style.backgroundColor = 'green';
+      gameNumbers[i] = 0;
+      updateNeighbors();
+    }
+  }
+}
+
+function updateNeighbors(){
+  console.log(clickCell);
+  if(clickCell === 0){
+    gameNumbers[clickCell + 1] = gameNumbers[clickCell + 1] + 1;
+    gameNumbers[clickCell + rowLength] = gameNumbers[clickCell + rowLength] + 1;
+  }
+  else if(clickCell === rowLength){
+    gameNumbers[clickCell - 1] = gameNumbers[clickCell - 1] + 1;
+    gameNumbers[clickCell + rowLength] = gameNumbers[clickCell + rowLength] + 1;
+  }
+  else if(clickCell === bottomCells[(rowLength * rowLength) - (rowLength)]){
+    gameNumbers[clickCell + 1] = gameNumbers[clickCell + 1] + 1;
+    gameNumbers[clickCell - rowLength] = gameNumbers[clickCell - rowLength] + 1;
+  }
+  else if(clickCell === rowLength * rowLength){
+    gameNumbers[clickCell - 1] = gameNumbers[clickCell - 1] + 1;
+    gameNumbers[clickCell - rowLength] = gameNumbers[clickCell - rowLength] + 1;
+  }
+  else if(leftCells.includes(clickCell)){
+    gameNumbers[clickCell + 1] = gameNumbers[clickCell + 1] + 1;
+    gameNumbers[clickCell + rowLength] = gameNumbers[clickCell + rowLength] + 1;
+    gameNumbers[clickCell - rowLength] = gameNumbers[clickCell - rowLength] + 1;
+  }
+  else if(rightCells.includes(clickCell)){
+    gameNumbers[clickCell - 1] = gameNumbers[clickCell - 1] + 1;
+    gameNumbers[clickCell + rowLength] = gameNumbers[clickCell + rowLength] + 1;
+    gameNumbers[clickCell - rowLength] = gameNumbers[clickCell - rowLength] + 1;
+  }
+  else if(topCells.includes(clickCell)){
+    gameNumbers[clickCell + 1] = gameNumbers[clickCell + 1] + 1;
+    gameNumbers[clickCell - 1] = gameNumbers[clickCell - 1] + 1;
+    gameNumbers[clickCell + rowLength] = gameNumbers[clickCell + rowLength] + 1;
+  }
+  else if(bottomCells.includes(clickCell)){ console.log(clickCell);
+    gameNumbers[clickCell + 1] = gameNumbers[clickCell + 1] + 1;
+    gameNumbers[clickCell - 1] = gameNumbers[clickCell - 1] + 1;
+    gameNumbers[clickCell - rowLength] = gameNumbers[clickCell - rowLength] + 1;
+  }
+  else {
+    gameNumbers[clickCell + 1] = gameNumbers[clickCell + 1] + 1;
+    gameNumbers[clickCell - 1] = gameNumbers[clickCell - 1] + 1;
+    gameNumbers[clickCell + rowLength] = gameNumbers[clickCell + rowLength] + 1;
+    gameNumbers[clickCell - rowLength] = gameNumbers[clickCell - rowLength] + 1;
+  }
+  clearAndCheck();
+}
+
+game.addEventListener('click', updateNumbers);
 
 makeGameTable();
 edgeCells();
