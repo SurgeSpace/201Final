1162267aecb00d5374ee082006bc5a9e5b23ed40
Steven Starwalt <steven.starwalt@yahoo.com>
2018-01-30 11:39:27 -0800

    got the game working for all neighbor cells

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 var game = document.getElementById('gameTable');
 // var numbers = document.getElementById('numberTable');
 
-var gameSize = 3;
+var gameSize = 5;
 
 var topIndex = parseInt(gameSize - 1);
 
@@ -14,6 +14,8 @@ var gameNumbers = [];
 var gameIndex = 0;
 
 var cellNumber = -1;
+
+var clearedCells = [];
 
 var topCells = [];
 var rightCells = [];
@@ -97,7 +99,10 @@ function updateNumbers(event){
 function clearAndCheck(){
   for(var i in gameNumbers){
     if(gameNumbers[i] > 3){
-      event.target.style.display = 'none';
+      clickCell = parseInt(i);
+      clearedCells.push(i);
+      var currentIndex = i;
+      document.getElementById(currentIndex).style.display = 'none';
       gameNumbers[i] = 0;
       updateNeighbors();
     }
@@ -166,6 +171,11 @@ function updateNeighbors(){
     console.log('i');
   }
   console.log(gameNumbers);
+  clearAndCheck();
+  for(var i in clearedCells){
+    console.log(clearedCells);
+    gameNumbers[clearedCells[i]] = 0;
+  }
 }
 
 function rightCell(){
