48228c70130bcbbdc17709832b3794fac661cd13
RebeccaJ21 <rebecca.prows@gmail.com>
2018-01-30 16:45:55 -0800

    cssDay2

--- js/index.js
+++ js/index.js
@@ -4,22 +4,19 @@
 function store(){
   if (!localStorage.firstName) {
     var inputFirstName = document.getElementById('firstName');
-    var inputLastName = document.getElementById('lastName');
     localStorage.setItem('firstName', inputFirstName.value);
-    localStorage.setItem('lastName', inputLastName.value);
+   
   }
 }
 
 function hideEl() {
   document.getElementById('firstName').style.display = 'none';
-  document.getElementById('lastName').style.display = 'none';
-  document.getElementById('lText').style.display = 'none';
   document.getElementById('fText').style.display = 'none';
 }
 
 function pageLoad() {
   if (localStorage.firstName) {
-    document.getElementById('welcome').innerHTML = 'Welcome ' + localStorage.firstName + ' ' + localStorage.lastName;
+    document.getElementById('welcome').innerHTML = 'Welcome ' + localStorage.firstName;
     hideEl();
   }
 }
