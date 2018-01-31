fe7a907b6ca7802c5913dba60ceba1d729caea7f
Jeremy Crawford <jfcraw08@gmail.com>
2018-01-31 14:12:52 -0800

    scores table ready for localStorage variables and sync efforts

--- js/scores.js
+++ js/scores.js
@@ -4,6 +4,11 @@ var scoresTable = document.getElementById('scoreTable');
 var trEl = document.createElement('tr');
 var tdEl = document.createElement('td');
 
+var refresh = document.getElementById('restartGame');
+var refreshButton = document.createElement('button');
+refreshButton.textContent = 'Play again!';
+refresh.appendChild(refreshButton);
+
 High.users = [];
 
 function High (userName, level, scored) {
@@ -56,19 +61,24 @@ function yourScore() {
   pEl.textContent = 'Current Score: ' + userScore;
 }
 
+if(localStorage.previousHighScores){
+  High.users = JSON.parse(localStorage.getItem('previousHighScores'));
+} else {
+  new High('SRJJ', 9, 15000);
+  new High('Joe', 8, 13000);
+  new High('Becky',7, 11000);
+  new High('Steph', 6, 9000);
+  new High('Jim', 5, 7000);
+}
 
-new High('SRJJ', 9, 15000);
-new High('Joe', 8, 13000);
-new High('Becky',7, 11000);
-new High('Steph', 6, 9000);
-new High('Jim', 5, 7000);
+// var userName = localStorage.userName;
+// var userLevel = JSON.parse(localStorage.getItem('lastGamePlayed'));
+// var userScore = JSON.parse(localStorage.getItem('currentScore'));
+//new High(userName, userLevel, userScore);
 
-
-
-var userName = 'Booger';
+var userName = 'Mikey';
 var userLevel = 10;
 var userScore = 15000;
-//new High(userName, userLevel, userScore);
 
 
 function checkScores() {
@@ -78,7 +88,7 @@ function checkScores() {
     userLevel = userLevel -= 1;
     userScore = userScore -= 1;
   }
-  if(userScore >= High.users[1].scored) {
+  if(userScore >= High.users[1].scored && userScore <= High.users[0].scored) {
     High.users[4].userName = High.users[3].userName;
     High.users[4].level = High.users[3].level;
     High.users[4].scored = High.users[3].scored;
@@ -130,6 +140,14 @@ function checkScores() {
   }
 }
 
+function clickRefreshButton(e) {
+  localStorage.previousHighScores = JSON.stringify(High.users);
+  localStorage.lastGamePlayed = 0;
+  localStorage.currentScore = 0;
+  location.href = '../html/game.html';
+}
+
+refreshButton.addEventListener('click', clickRefreshButton);
 
 yourScore();
 checkScores();
